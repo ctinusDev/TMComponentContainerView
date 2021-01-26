5c9fff9e17537d46e50f8fbd1728e6b4db89d1b7
tomychen <1158433594@qq.com>
2021-01-26 10:26:32 +0800

    Initial commit

---
+++ Example/TMComponentContainerView/TMViewController.h
@@ -0,0 +1,13 @@
+//
+//  TMViewController.h
+//  TMComponentContainerView
+//
+//  Created by ctinusDEV on 01/26/2021.
+//  Copyright (c) 2021 ctinusDEV. All rights reserved.
+//
+
+@import UIKit;
+
+@interface TMViewController : UIViewController
+
+@end
---
+++ Example/TMComponentContainerView/TMViewController.m
@@ -0,0 +1,29 @@
+//
+//  TMViewController.m
+//  TMComponentContainerView
+//
+//  Created by ctinusDEV on 01/26/2021.
+//  Copyright (c) 2021 ctinusDEV. All rights reserved.
+//
+
+#import "TMViewController.h"
+
+@interface TMViewController ()
+
+@end
+
+@implementation TMViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
